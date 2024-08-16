877b9455b9aa463ae08e738868b88fff1adadc85
Jihye <wlgul817@gmail.com>
2024-08-16 11:28:45 +0900

    버튼 대신 useEffect 사용하기

--- src/assets/pages/NewsPage.tsx
+++ src/assets/pages/NewsPage.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewsHeader from "../components/NewsHeader";
 import NewsList from "../components/NewsList";
 import axios from "axios";
-import { Button } from "@mui/material";
 
 const NewsPage = () => {
   const [data, setData] = useState({
@@ -24,35 +23,38 @@ const NewsPage = () => {
       },
     ],
   });
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=dcf6364c4e4448f8b370abfa27daa82e"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=dcf6364c4e4448f8b370abfa27daa82e"
+        );
+        setData(response.data);
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>대기 중...</div>;
+  }
+
+  if (!articles) {
+    return null;
+  }
 
   return (
     <>
       <NewsHeader />
       <NewsList news={data} />
-      <div>
-        <Button variant="contained" disableElevation onClick={onClick}>
-          불러오기
-        </Button>
-      </div>
-      <div>
-        {data && (
-          <textarea
-            rows={7}
-            value={JSON.stringify(data, null, 2)}
-            readOnly={true}
-          ></textarea>
-        )}
-      </div>
     </>
   );
 };
