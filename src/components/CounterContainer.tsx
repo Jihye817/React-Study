f61e64572e489cf0bab3f4251c86b465713df411
Jihye <wlgul817@gmail.com>
2024-08-24 16:39:17 +0900

    hook 이용하기

--- src/components/CounterContainer.tsx
+++ src/components/CounterContainer.tsx
@@ -1,31 +1,19 @@
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Counter from "./Counter";
 import { decrease, increase } from "../modules/counter";
-import { Dispatch } from "redux";
+import { useCallback } from "react";
 
-interface Props {
-  number: number;
-  increase: () => void;
-  decrease: () => void;
-}
+const CounterContainer = () => {
+  const number = useSelector(
+    (state: { counter: { number: number } }) => state.counter.number
+  );
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
 
-const CounterContainer = ({ number, increase, decrease }: Props) => {
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-const mapStateToProps = (state: { counter: { number: number } }) => ({
-  number: state.counter.number,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
